99ad6844b6ad9b90779dea11a834176f4aa6ab38
Medro Hi Tech Symbol Pvt Ltd <info@themedro.com>
2025-11-21 15:56:21 +0530

    Update start_docker.sh

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker stop reactapp || true
-docker rm reactapp || true
+docker stop upskillway || true
+docker rm upskillway || true
 docker pull 488479524661.dkr.ecr.ap-south-1.amazonaws.com/upskillway:latest
 docker run -d -p 80:80 --name upskillway 488479524661.dkr.ecr.ap-south-1.amazonaws.com/upskillway:latest
