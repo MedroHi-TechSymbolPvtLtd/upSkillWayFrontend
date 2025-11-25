4bcc0d05bfd230b602aa7358fac962fbd5ec4334
Medro Hi Tech Symbol Pvt Ltd <info@themedro.com>
2025-11-25 11:43:31 +0530

    Update stop_docker.sh

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+echo "Stopping old container if running..."
 docker stop upskillway || true
+
+echo "Removing old container..."
 docker rm upskillway || true
+
+echo "Pruning unused Docker resources..."
+docker system prune -f || true
