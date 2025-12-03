b95849cd0bf5a898003f14fbb934057ed135aec1
Medro Hi Tech Symbol Pvt Ltd <info@themedro.com>
2025-12-03 12:30:39 +0530

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Clean old deployment files before copying
+sudo rm -rf /home/ec2-user/app/scripts
+sudo rm -f /home/ec2-user/app/Dockerfile
+sudo rm -f /home/ec2-user/app/nginx.conf
 
-rm -rf /home/ec2-user/app/scripts
-rm -f /home/ec2-user/app/Dockerfile
-rm -f /home/ec2-user/app/nginx.conf
+# recreate scripts folder to prevent errors
+sudo mkdir -p /home/ec2-user/app/scripts
+sudo chown ec2-user:ec2-user /home/ec2-user/app/scripts
